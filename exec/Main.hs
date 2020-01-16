ba9ab017a4a2a8f9f4a74bac6c6704a269b77574
Colin Woodbury <colin@fosskers.ca>
2020-01-16 15:19:26 -0800

    Ingest the header stream into a db table
    A first pass. A few fields are missing.

--- exec/Main.hs
+++ exec/Main.hs
@@ -1,15 +1,28 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE DeriveAnyClass     #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE TypeApplications   #-}
 
 {-# OPTIONS_GHC -fno-warn-orphans #-}  -- TODO Remove if orphan is adopted.
 
 module Main ( main ) where
 
-import           Chainweb.Api.BlockHeader (BlockHeader)
+import           Chainweb.Api.BlockHeader (BlockHeader(..))
+import           Chainweb.Api.BytesLE
+import           Chainweb.Api.ChainId (ChainId(..))
+import           Chainweb.Api.Hash
+import           ChainwebDb.Types.Block (BlockT(..))
+import           ChainwebDb.Types.DbHash (DbHash(..))
+import           Control.Exception (bracket)
 import           Data.Aeson (ToJSON(..), object)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
-import           Lens.Micro ((&), (^?))
+import           Data.Time.Clock.POSIX (posixSecondsToUTCTime)
+import           Database.Beam
+import           Database.Beam.Sqlite
+import           Database.SQLite.Simple
+import           Lens.Micro ((^?))
 import           Lens.Micro.Aeson (key, _JSON)
 import           Network.HTTP.Client
 import           Network.HTTP.Client.TLS (tlsManagerSettings)
@@ -17,17 +30,33 @@ import           Network.Wai.EventSource.Streaming
 import           Servant.Client.Core (BaseUrl(..), Scheme(..))
 import qualified Streaming.Prelude as SP
 
+
 ---
 
 main :: IO ()
 main = do
   m <- newManager tlsManagerSettings
-  headerStream m $ BaseUrl Https "honmono.fosskers.ca" 443 ""
+  bracket (open "chainweb-data.db") close $ ingest m url
+  where
+    url = BaseUrl Https "honmono.fosskers.ca" 443 ""
 
-headerStream :: Manager -> BaseUrl -> IO ()
-headerStream m u = withEvents (req u) m $ \updates -> updates
-  & dataOnly @BlockHeader
-  & SP.print
+ingest :: Manager -> BaseUrl -> Connection -> IO ()
+ingest m u c = withEvents (req u) m $ SP.mapM_ f . dataOnly @BlockHeader
+  where
+    f :: BlockHeader -> IO ()
+    f bh = runBeamSqlite c . runInsert . insert (blocks database) $ insertExpressions [g bh]
+
+    g :: BlockHeader -> BlockT (QExpr Sqlite s)
+    g bh = Block
+      { _block_id           = default_
+      , _block_creationTime = val_ . posixSecondsToUTCTime $ _blockHeader_creationTime bh
+      , _block_chainId      = val_ . unChainId $ _blockHeader_chainId bh
+      , _block_height       = val_ $ _blockHeader_height bh
+      , _block_hash         = val_ . DbHash . hashB64U $ _blockHeader_hash bh
+      , _block_target       = val_ . DbHash . hexBytesLE $ _blockHeader_target bh
+      , _block_weight       = val_ . DbHash . hexBytesLE $ _blockHeader_weight bh
+      , _block_epochStart   = val_ . posixSecondsToUTCTime $ _blockHeader_epochStart bh
+      , _block_nonce        = val_ $ _blockHeader_nonce bh }
 
 req :: BaseUrl -> Request
 req u = defaultRequest
@@ -46,3 +75,14 @@ instance FromEvent BlockHeader where
 
 instance ToJSON BlockHeader where
   toJSON _ = object []
+
+--------------------------------------------------------------------------------
+-- Database Definition
+
+data ChainwebDataDb f = ChainwebDataDb
+  { blocks :: f (TableEntity BlockT) }
+  deriving stock (Generic)
+  deriving anyclass (Database be)
+
+database :: DatabaseSettings be ChainwebDataDb
+database = defaultDbSettings
