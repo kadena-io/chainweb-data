2ce45de0fcafcb831d3fb86da626fa74a4b3185b
Emily Pillmore <emilypi@cohomolo.gy>
2020-09-10 17:45:31 -0400

    amend script

--- scripts/richlist.sh
+++ scripts/richlist.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-DIR=$1;
 
 for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
 do
-   DB=$DIR/chainweb-node/mainnet01/0/sqlite/pact-v1-chain-$i.sqlite
-
-   sqlite3 -header -csv $DB "select rowkey as acct_id, txid, cast(ifnull(json_extract(rowdata, '$.balance.decimal'), json_extract(rowdata, '$.balance')) as REAL) as 'balance'
+   sqlite3 -header -csv pact-v1-chain-$i.sqlite "select rowkey as acct_id, txid, cast(ifnull(json_extract(rowdata, '$.balance.decimal'), json_extract(rowdata, '$.balance')) as REAL) as 'balance'
      from 'coin_coin-table' as coin
      INNER JOIN (
       select
