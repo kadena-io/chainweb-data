852fe62dd4be6b296f6130a90be2a6aaf243751f
Doug Beardsley <mightybyte@gmail.com>
2022-01-25 23:13:49 -0500

    Make txhandler handle reintroduced txs 2 (#72)
    Co-authored-by: Emmanuel Denloye-Ito <eod8tb@virginia.edu>

--- default.nix
+++ default.nix
@@ -18,7 +18,19 @@ in haskellPackages.developPackage {
     #  sha256 = "1a70da15hb4nlpxhnsy1g89frbpf3kg3mwb4g9carj5izw1w1r1k";
     #} {};
 
+    direct-sqlite = dontCheck (self.callHackageDirect {
+      pkg = "direct-sqlite";
+      ver = "2.3.26";
+      sha256 = "1kdkisj534nv5r0m3gmxy2iqgsn6y1dd881x77a87ynkx1glxfva";
+    } {});
+
     pact = dontCheck super.pact;
+
+    pact-time = dontCheck (self.callHackageDirect {
+      pkg = "pact-time";
+      ver = "0.2.0.0";
+      sha256 = "1cfn74j6dr4279bil9k0n1wff074sdlz6g1haqyyy38wm5mdd7mr";
+    } {});
   };
   source-overrides = {
     beam-automigrate = nix-thunk.thunkSource ./deps/beam-automigrate;
