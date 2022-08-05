471e7eed6a3d638e9747b6ba08afb95591ba19db
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2022-08-05 11:43:50 -0400

    get basic scaffolding down

--- exec/Chainweb/RichListScript.hs
+++ exec/Chainweb/RichListScript.hs
@@ -1,18 +1,65 @@
 {-# language LambdaCase #-}
+{-# language TypeApplications #-}
 module Chainweb.RichListScript where
 
 import Control.Exception
-import Control.Monad (forM)
+import Control.Monad
+import Data.List (isPrefixOf, sort)
 import qualified Data.Text as T
 import Data.Text (Text)
 import Data.String.Conv
 import Data.String
+
+import System.Directory
+import System.FilePath
 import System.Exit (die)
+import System.Logger.Types
+
+import Text.Read
 
 import Database.SQLite3
 import Database.SQLite3.Direct (Utf8(..))
 
 import Pact.Types.SQLite
+
+
+richList :: LogFunctionIO Text -> FilePath -> IO ()
+richList logger fp = do
+
+    files <- doesPathExist fp >>= \case
+        True -> checkChains
+        False -> ioError $ userError
+          $ "Chainweb-node top-level db directory does not exist: "
+          <> fp
+    logger Info "Aggregating richlist ..."
+    forM_ files $ \file -> withSQLiteConnection file $ undefined
+  where
+    checkChains :: IO [FilePath]
+    checkChains = do
+        let sqlitePath = fp <> "chainweb-node/mainnet01/0/sqlite" -- TODO: notice this only works for mainnet
+
+        doesPathExist sqlitePath >>= \case
+          False -> ioError $ userError "Cannot find sqlite data. Is your node synced?"
+          True -> do
+            files <- filter ((==) ".sqlite" . takeExtension) <$> listDirectory sqlitePath
+
+            let go p
+                  | "pact-v1-chain-" `isPrefixOf` p =
+                    case splitAt 14 (fst $ splitExtension p) of
+                      (_, "") -> error $ "Found corrupt sqlite path: " <> p
+                      (_, cid) -> case readMaybe @Int cid of
+                        Just c -> ((p:), (c:))
+                        Nothing -> error "Couldn't read chainid"
+                  | otherwise = mempty
+                (fdl, cdl) = foldMap go files
+                chains = cdl []
+                isConsecutive = all (\(x,y) -> succ x == y)
+                  . (zip <*> tail)
+                  . sort
+            unless (isConsecutive chains)
+              $ ioError $ userError
+              $ "Missing tables for some chain ids. Is your node synced?"
+            return (fdl [])
 
 -- WARNING: This function will throw errors if found. We don't "catch" errors in an Either type
 withSQLiteConnection :: FilePath -> (Database -> IO a) -> IO a
