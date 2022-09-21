097cd745ca13f389cee7220b141d765da4635dde
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2022-09-21 19:07:09 -0400

    specify correct HasColumnType instance for KDAScientific

--- lib/ChainwebDb/Types/Transfer.hs
+++ lib/ChainwebDb/Types/Transfer.hs
@@ -67,18 +67,9 @@ instance Table TransferT where
     deriving anyclass (Beamable)
   primaryKey = TransferId <$> _tr_block <*> _tr_requestkey <*> _tr_chainid <*> _tr_idx <*> _tr_moduleHash
 
-{-
-
-The two values in given to the function numericType correspond to the precision
-and scale of the numeric type. Read here for more information
-https://www.postgresql.org/docs/current/datatype-numeric.html.
-
-Since the maximum amount of kda someone can have in their posssession is around
-1 billion and also the maximum precision of any number represented by pact needs
-12 digits past the decimal point, we have chosen the numbers 21 and 12 for the
-precision and scale of the newtype KDAScientific.
-
--}
-
 newtype KDAScientific = KDAScientific { getKDAScientific :: Scientific }
-  deriving newtype (Eq, Show, BA.HasColumnType, HasSqlValueSyntax PgValueSyntax, ToField, FromField, FromBackendRow Postgres)
+  deriving newtype (Eq, Show, HasSqlValueSyntax PgValueSyntax, ToField, FromField, FromBackendRow Postgres)
+
+instance BA.HasColumnType KDAScientific where
+  defaultColumnType _ = BA.SqlStdType (numericType Nothing)
+  defaultTypeCast _ = Just "numeric"
