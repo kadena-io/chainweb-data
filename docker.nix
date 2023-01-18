13fd90e07c86ca65f828db5fabbd20fec1d58f4a
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2023-01-18 16:26:53 -0500

    use ubuntu as a base image

--- docker.nix
+++ docker.nix
@@ -1,8 +1,8 @@
 { kpkgs ? import ./deps/kpkgs {}
 , chainweb-data ? import ./. {}
 , dockerTag ? "latest"
-, rev ? "7e9b0dff974c89e070da1ad85713ff3c20b0ca97"
-, sha256 ? "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36"
+, rev ? "a7ecde854aee5c4c7cd6177f54a99d2c1ff28a31"
+, sha256 ? "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02"
 , pkgs ?
   import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
@@ -13,14 +13,23 @@
   }
 , ... }:
 
-kpkgs.pkgs.dockerTools.buildImage {
+let ubuntuFromDockerHub = pkgs.dockerTools.pullImage {
+  imageName = "ubuntu";
+  imageDigest = "sha256:965fbcae990b0467ed5657caceaec165018ef44a4d2d46c7cdea80a9dff0d1ea";
+  sha256 = "04amkx2mgwzzf6r14fgn4x38s69s0rxmm8ik2cnh911cdbqvwbn1";
+  finalImageTag = "22.04";
+  finalImageName = "ubuntu";
+};
+in pkgs.dockerTools.buildImage {
   name = "chainweb-data-docker";
   tag = dockerTag;
-  contents = [pkgs.postgresql_12 pkgs.curl pkgs.sqlite];
+  contents = [pkgs.postgresql_12 pkgs.curl pkgs.sqlite pkgs.bash pkgs.coreutils];
+
+  fromImage = ubuntuFromDockerHub;
 
   runAsRoot = ''
-    #!${kpkgs.pkgs.runtimeShell}
-    ${kpkgs.pkgs.dockerTools.shadowSetup}
+    #!${pkgs.runtimeShell}
+    ${pkgs.dockerTools.shadowSetup}
     mkdir -p /chainweb-data
     '';
 
@@ -28,7 +37,7 @@ kpkgs.pkgs.dockerTools.buildImage {
     Cmd = [ "--help"];
     WorkingDir = "/chainweb-data";
     Volumes = { "/chainweb-data" = {}; };
-    Entrypoint = [ "${kpkgs.pkgs.haskell.lib.justStaticExecutables chainweb-data}/bin/chainweb-data" ];
+    Entrypoint = [ "${pkgs.haskell.lib.justStaticExecutables chainweb-data}/bin/chainweb-data" ];
   };
 
 }
