632af4e6fed2fa7db6bd9bf4f291e67b577f31ec
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2023-02-02 14:19:49 -0500

    result after session with enis.. builds everything correctly!🎉

--- flake.nix
+++ flake.nix
@@ -2,12 +2,19 @@
   description = "Data ingestion for Chainweb";
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs?rev=7a94fcdda304d143f9a40006c033d7e190311b54";
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=4d2b37a84fad1091b9de401eb450aae66f1a741e";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     haskellNix.url = "github:input-output-hk/haskell.nix";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
+  nixConfig = {
+    # This sets the flake to use the IOG nix cache.
+    # Nix should ask for permission before using it,
+    # but remove it here if you do not want it to.
+    extra-substituters = ["https://cache.iog.io"];
+    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    allow-import-from-derivation = "true";
+  };
 
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
     flake-utils.lib.eachSystem
@@ -21,24 +28,23 @@
           flake = pkgs.chainweb-data.flake {
 
           };
+          executable = (flake.packages."chainweb-data:exe:chainweb-data").override { dontStrip = false; };
           overlays = [ haskellNix.overlay
             (final: prev: {
 	    chainweb-data =
 	      final.haskell-nix.project' {
-	        src = ./haskell-src;
+	        src = ./.;
 	        compiler-nix-name = "ghc8107";
-                cabalProject = "cabal.project";
+                projectFileName = "cabal.project";
 	        shell.tools = {
 	          cabal = {};
 	        };
-	        shell.buildInputs = with pkgs; [
-
-	        ];
 	      };
 
             })
           ];
-        in flake // {
-          packages.default = flake.packages."chainweb-data:exe:chainweb-data";
+        in  flake // {
+          packages.default = executable; 
+          packages.chainweb-data-docker = import ./docker.nix { inherit pkgs; chainweb-data = executable; };
           });
 }
