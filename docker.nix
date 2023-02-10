3c9e59d3a48bff8164242b0d7793537f35fdd449
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2023-02-10 18:25:14 -0500

    don't call nix-build docker.nix
    Only use nix build .#"chainweb-data-docker" to build docker image!

--- docker.nix
+++ docker.nix
@@ -1,17 +1,7 @@
-{ kpkgs ? import ./deps/kpkgs {}
+{ pkgs
 , chainweb-data ? import ./.
 , dockerTag ? "latest"
-, rev ? "a7ecde854aee5c4c7cd6177f54a99d2c1ff28a31"
-, sha256 ? "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02"
-, pkgs ?
-  import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    inherit sha256;
-  }) {
-    config.allowBroken = false;
-    config.allowUnfree = true;
-  }
-, baseImage ? pkgs.dockerTools.pullImage {
+, baseImageDef ? {
       imageName = "ubuntu";
       imageDigest = "sha256:965fbcae990b0467ed5657caceaec165018ef44a4d2d46c7cdea80a9dff0d1ea";
       sha256 = "10wlr8rhiwxmz1hk95s9vhkrrjkzyvrv6nshg23j86rw08ckrqnz";
@@ -20,7 +10,8 @@
       }
 , ... }:
 
-pkgs.dockerTools.buildImage {
+let baseImage = pkgs.dockerTools.pullImage baseImageDef;
+in pkgs.dockerTools.buildImage {
   name = "chainweb-data";
   tag = dockerTag;
 
