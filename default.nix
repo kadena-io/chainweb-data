1edcd196240bebb7e3707f9055e4e54d37610454
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2023-02-22 14:44:18 +0100

    Make threading&profiling optional in default.nix

--- default.nix
+++ default.nix
@@ -2,9 +2,16 @@ let inputs = import ./flake-inputs.nix;
     pkgsDef = import inputs.nixpkgs (import inputs.haskellNix {}).nixpkgsArgs;
 in
 { pkgs ? pkgsDef
+, dontStrip ? false
+, threaded ? true
+, enableProfiling ? false
 , ...
 }:
-let chainweb-data = pkgs.haskell-nix.project' {
+let profilingModule = {
+      enableLibraryProfiling = enableProfiling;
+      enableProfiling = enableProfiling;
+    };
+    chainweb-data = pkgs.haskell-nix.project' {
 	    src = ./.;
       index-state = "2023-02-01T00:00:00Z";
 	    compiler-nix-name = "ghc8107";
@@ -12,9 +19,15 @@ let chainweb-data = pkgs.haskell-nix.project' {
 	    shell.tools = {
 	      cabal = {};
 	    };
+      modules = if enableProfiling then [ profilingModule ] else [];
     };
     flake = chainweb-data.flake {};
-    default = flake.packages."chainweb-data:exe:chainweb-data".override { dontStrip = false; };
+    default = flake.packages."chainweb-data:exe:chainweb-data".override (old: {
+      inherit dontStrip;
+      flags = old.flags // {
+        inherit threaded;
+      };
+    });
 in {
   inherit flake default;
 }
