5e21686370166b778f3072e4bb4de779b40b39ee
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2023-02-23 19:21:40 +0100

    Nix reorganization (#136)
    This PR turns our Nix configuration somewhat inside out and moves all the heavy lifting to `default.nix` from `flake.nix`.
    
    The rationale is that Nix flakes are inflexible by design so that they provide static information about their inputs and their outputs. By moving the interesting logic back to `default.nix` and importing it from `flake.nix` we're able to accept arguments to the `nix-build` call to build our artifacts with parameters.
    
    This PR introduces the `dontStrip`, `threaded` and `enableProfiling` parameters to `default.nix`.
    
    In order to expose this new flexibility to our `chanweb-data-docker` images, this PR also inlines `docker-legacy.nix` into `default.nix` so that, say, `nix-build --arg enableProfiling true -A dockerImage` builds a docker image with a CW-D binary built with profiling.
    
    Note that this `default.nix` still piggy backs on the new nix flakes feature for managing its inputs like `haskell.nix` and `nixpkgs`. 
    
    * Get docker.nix inputs from flake-compat
    
    * Move project definition to default.nix
    
    * fix docker-legacy.nix
    
    * Make threading&profiling optional in default.nix
    
    * inline docker-legacy.nix and flake-inputs.nix into default.nix
    
    ---------
    
    Co-authored-by: Emmanuel Denloye-Ito <eod8tb@virginia.edu>

--- default.nix
+++ default.nix
@@ -1,7 +1,65 @@
-(import (
-  fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/35bb57c0c8d8b62bbfd284272c928ceb64ddbde9.tar.gz";
-    sha256 = "1prd9b1xx8c0sfwnyzkspplh30m613j42l1k789s521f4kv4c2z2"; }
-) {
-  src =  ./.;
-}).defaultNix
+let inputs = (import (
+      fetchTarball {
+         url = "https://github.com/edolstra/flake-compat/archive/35bb57c0c8d8b62bbfd284272c928ceb64ddbde9.tar.gz";
+         sha256 = "1prd9b1xx8c0sfwnyzkspplh30m613j42l1k789s521f4kv4c2z2"; }
+     ) {
+       src =  ./.;
+     }).defaultNix.inputs;
+    pkgsDef = import inputs.nixpkgs (import inputs.haskellNix {}).nixpkgsArgs;
+in
+{ pkgs ? pkgsDef
+, dontStrip ? false
+, threaded ? true
+, enableProfiling ? false
+, dockerTag ? "latest"
+, baseImageDef ? {
+      imageName = "ubuntu";
+      imageDigest = "sha256:965fbcae990b0467ed5657caceaec165018ef44a4d2d46c7cdea80a9dff0d1ea";
+      sha256 = "10wlr8rhiwxmz1hk95s9vhkrrjkzyvrv6nshg23j86rw08ckrqnz";
+      finalImageTag = "22.04";
+      finalImageName = "ubuntu";
+      }
+, ...
+}:
+let profilingModule = {
+      enableLibraryProfiling = enableProfiling;
+      enableProfiling = enableProfiling;
+    };
+    chainweb-data = pkgs.haskell-nix.project' {
+	    src = ./.;
+      index-state = "2023-02-01T00:00:00Z";
+	    compiler-nix-name = "ghc8107";
+      projectFileName = "cabal.project";
+	    shell.tools = {
+	      cabal = {};
+	    };
+      modules = if enableProfiling then [ profilingModule ] else [];
+    };
+    flake = chainweb-data.flake {};
+    default = flake.packages."chainweb-data:exe:chainweb-data".override (old: {
+      inherit dontStrip;
+      flags = old.flags // {
+        inherit threaded;
+      };
+    });
+    baseImage = pkgs.dockerTools.pullImage baseImageDef;
+    dockerImage = pkgs.dockerTools.buildImage {
+       name = "chainweb-data";
+       tag = dockerTag;
+
+       fromImage = baseImage;
+
+       runAsRoot = ''
+         ln -s "${default}/bin/chainweb-data" /usr/local/bin/
+         mkdir -p /chainweb-data
+         '';
+
+       config = {
+         WorkingDir = "/chainweb-data";
+         Volumes = { "/chainweb-data" = {}; };
+         Entrypoint = [ "chainweb-data" ];
+       }; 
+    };
+in {
+  inherit flake default dockerImage;
+}
