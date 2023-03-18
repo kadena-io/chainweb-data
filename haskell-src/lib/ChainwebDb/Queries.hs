kadena-io/chainweb-data

commit 81850344e3130ba8900dc783cc8ce9627875710d

Gather XChain info for transfers (#137)
This PR extends the transfer search queries to attach cross-chain information to cross-chain transfer rows. For the send-side of cross-chain transactions it extracts the intended receiver from the corresponding `TRANSFER_XCHAIN` event of the same transaction and for the receive-side, it chases the `pactid` of the associated transaction and looks for a matching `TRANSFER_XCHAIN`. 

* Pin xchain info chainweb-api commit

* Join cross-chain toAcct to transfers

* Improve the gather subquery

* Implement xChainInfo for both directions

* Fix typo in comments

* Rename xchain -> crossChain

haskell-src/lib/ChainwebDb/Queries.hs
@@ -216,8 +216,10 @@ transfersSearchSource tsp = do
       return tr
     sourceTransfersScan = unionAll_ (accountQuery _tr_from_acct) (accountQuery _tr_to_acct)
 
-newtype TransferSearchExtrasT f = TransferSearchExtras
+data TransferSearchExtrasT f = TransferSearchExtras
   { tseBlockTime :: C f UTCTime
+  , tseXChainAccount :: C f (Maybe Text)
+  , tseXChainId :: C f (Maybe Int64)
   } deriving (Generic, Beamable)
 
 transferSearchExtras ::
@@ -226,9 +228,66 @@ transferSearchExtras ::
 transferSearchExtras tr = do
   blk <- all_ $ _cddb_blocks database
   guard_ $ _tr_block tr `references_` blk
+  xChainInfo <- joinXChainInfo tr
   return $ TransferSearchExtras
     { tseBlockTime = _block_creationTime blk
+    , tseXChainAccount = xciAccount xChainInfo
+    , tseXChainId = xciChainId xChainInfo
     }
+
+data XChainInfoT f = XChainInfo
+  { xciAccount :: C f (Maybe Text)
+  , xciChainId :: C f (Maybe Int64)
+  } deriving (Generic, Beamable)
+
+joinXChainInfo :: TransferT (PgExpr s) ->
+  Q Postgres ChainwebDataDb s (XChainInfoT (PgExpr s))
+joinXChainInfo tr = pgUnnest $ (customExpr_ $ \fromAcct toAcct idx mdName blk req amt ->
+  -- We need the following LATERAL keyword so that it can be used liberally
+  -- in any Q context despite the fact that it refers to the `tr` coming
+  -- from the outside scope. The LATERAL helps, because when the expression below
+  -- appears after a XXXX JOIN, this LATERAL prefix will turn it into a lateral
+  -- join. This is very hacky, but Postgres allows the LATERAL keyword after FROM
+  -- as well, so I can't think of a case that would cause the hack to blow up.
+  -- Either way, once we have migrations going, we should replace this body with
+  -- a Postgres function call, which the pgUnnest + customExpr_ combination was
+  -- designed for.
+  " LATERAL ( " <>
+    " SELECT e.params->>1 AS acct, e.chainid " <>
+    " FROM events e " <>
+    " WHERE e.block = " <> blk <>
+      " AND e.requestkey = " <> req <>
+      " AND e.qualname = 'coin.TRANSFER_XCHAIN' " <>
+      " AND e.params->>0 = " <> fromAcct <>
+      " AND " <> toAcct <> " = '' " <>
+      " AND e.params->>2 = CAST(" <> amt <> "AS VARCHAR) " <>
+      " AND e.idx = " <> idx <> " - 1 " <>
+      " AND " <> mdName <> " = 'coin' " <>
+    " UNION ALL " <>
+    " SELECT e.params->>0 AS acct, e.chainid " <>
+    " FROM transactions tRec, transactions tSend, events e " <>
+    " WHERE " <> mdName <> " = 'coin' " <>
+      " AND " <> req <> " != 'cb' " <>
+      " AND " <> fromAcct <> " = '' " <>
+      " AND tRec.block = " <> blk <>
+      " AND tRec.requestkey = " <> req <>
+      " AND tSend.requestkey = tRec.pactid " <>
+      " AND e.block = tSend.block " <>
+      " AND e.requestkey = tSend.requestkey " <>
+      " AND e.qualname = 'coin.TRANSFER_XCHAIN' " <>
+      " AND e.params->>1 = " <> toAcct <>
+      " AND e.params->>2 = CAST(" <> amt <> "AS VARCHAR) " <>
+    " UNION ALL " <>
+    " SELECT NULL, NULL " <>
+    " LIMIT 1 " <>
+  ")")
+  (_tr_from_acct tr)
+  (_tr_to_acct tr)
+  (_tr_idx tr)
+  (_tr_modulename tr)
+  (unBlockId $ _tr_block tr)
+  (_tr_requestkey tr)
+  (_tr_amount tr)
 
 whenArg :: Monad m => Maybe a -> (a -> m ()) -> m ()
 whenArg p a = maybe (return ()) a p

cabal.project
@@ -18,8 +18,8 @@ source-repository-package
 source-repository-package
     type: git
     location: https://github.com/kadena-io/chainweb-api.git
-    tag: 8a4731c2875753617ccd2b573cf726fa100c6053
-    --sha256: sha256-MY470zn+BveL7X7gFVSgGqvWjD0jsk6VKRF/gzal9Bc=
+    tag: c877d32b46175917d9df9521d0c4e7ec47ebd5cd
+    --sha256: sha256-X6fQTcV46lHT3vifSDO0exnoYBr7Tx2YEpK8EyEFgBA=
 
 source-repository-package
     type: git

haskell-src/exec/Chainweb/Server.hs
@@ -589,6 +589,8 @@ accountHandler logger pool req account token chain minheight maxheight limit mbO
         , _trDetail_amount = StringEncoded $ getKDAScientific $ _tr_amount tr
         , _trDetail_fromAccount = _tr_from_acct tr
         , _trDetail_toAccount = _tr_to_acct tr
+        , _trDetail_crossChainAccount = tseXChainAccount extras
+        , _trDetail_crossChainId = fromIntegral <$> tseXChainId extras
         , _trDetail_blockTime = tseBlockTime extras
         }
 

commit 30cce2a9ddf9d0850806380b461eebf80e235f93

Fix the chainid of outgoing xchain info (#138)
This PR fixes a bug that was introduced with #137 where the chain ID of the xchain transfer info of cross chain transfers were mistakenly reported as the chain of the sender account.

haskell-src/lib/ChainwebDb/Queries.hs
@@ -253,7 +253,7 @@ joinXChainInfo tr = pgUnnest $ (customExpr_ $ \fromAcct toAcct idx mdName blk re
   -- a Postgres function call, which the pgUnnest + customExpr_ combination was
   -- designed for.
   " LATERAL ( " <>
-    " SELECT e.params->>1 AS acct, e.chainid " <>
+    " SELECT e.params->>1 AS acct, CAST(e.params->>3 AS INT) " <>
     " FROM events e " <>
     " WHERE e.block = " <> blk <>
       " AND e.requestkey = " <> req <>