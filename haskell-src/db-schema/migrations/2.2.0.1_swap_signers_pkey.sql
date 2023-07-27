e2f78cfc5058934be3647d7b65bbfe1f20e5f27d
Michael Williams <Michael.Williams@glexia.com>
2023-07-27 01:19:18 -0400

    Fix 2.2.0.1_swap_signers_pkey.sql (#155)
    Remove "cwd" schema prefix to correct migration.

--- haskell-src/db-schema/migrations/2.2.0.1_swap_signers_pkey.sql
+++ haskell-src/db-schema/migrations/2.2.0.1_swap_signers_pkey.sql
@@ -1,2 +1,2 @@
 ALTER TABLE signers DROP CONSTRAINT signers_pkey;
-ALTER TABLE cwd.signers ADD CONSTRAINT signers_pkey PRIMARY KEY (requestkey, idx);
+ALTER TABLE signers ADD CONSTRAINT signers_pkey PRIMARY KEY (requestkey, idx);
